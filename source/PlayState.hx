d1fd9901be41d4b15dc7ceacb10697bfcdd90e18
Bohdan Taraba <tbohdan@seznam.cz>
2015-03-05 22:46:48 +0100

    Revert "Revert "first game logic""
    This reverts commit 31303b80edb76869e256fb4b87116625bc819a65.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,33 +7,45 @@ import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.util.FlxMath;
 
-/**
- * A FlxState which can be used for the actual gameplay.
- */
+
+enum Turn
+{
+	CPU;
+	PLAYER;
+}
+
 class PlayState extends FlxState
 {
-	/**
-	 * Function that is called up when to state is created to set it up. 
-	 */
+	
+	var turn:Turn;
+	
 	override public function create():Void
 	{
 		super.create();
+		turn = Turn.PLAYER;
 	}
 	
-	/**
-	 * Function that is called when this state is destroyed - you might want to 
-	 * consider setting all objects this state uses to null to help garbage collection.
-	 */
 	override public function destroy():Void
 	{
 		super.destroy();
 	}
 
-	/**
-	 * Function that is called once every frame.
-	 */
 	override public function update():Void
 	{
 		super.update();
+		
+		switch(turn)
+		{
+			case Turn.PLAYER:
+				{
+					
+				}
+			case Turn.CPU:
+				{
+					
+				}
+		}
+		
+		
 	}	
 }
